ffa8af5d4693a65acff23abe33e3f3a4d5b43716
katsunori-osanai <katsu.gretsch@gmail.com>
2022-01-24 10:53:15 +0900

    feat(emotion): add Emotion
    ## やったこと
    
    - Emotion の導入

---
+++ src/styles/styles.ts
@@ -0,0 +1,21 @@
+export const space = {
+  large: '16px',
+  medium: '12px',
+  small: '8px'
+};
+
+export const color = {
+  primary: '#4aa0fb',
+  secondary: '#d7effe',
+  white: '#ffffff',
+
+  dartText: 'rgba(0, 0, 0, 0.9)',
+  lightText: 'rgba(0, 0, 0, 0.4)',
+
+  border: 'rgba(0, 0, 0, 0.1)',
+  activeBorder: 'rgba(0, 0, 0, 0.15)'
+};
+
+export const borderRadius = {
+  defalut: '4px'
+};
